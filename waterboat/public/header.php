0eead4bc45cd5d9cb1f95b85729dc4e8b856e063
vinhlp32aptech <vinh.lp32@aptechlearning.edu.vn>
2020-06-03 13:56:59 +0700

    them signinup

--- waterboat/public/header.php
+++ waterboat/public/header.php
@@ -89,6 +89,10 @@
                             <li>
                                 <a href="contact.php" class="nav-link text-left">Contact</a>
                             </li>
+                            <li>
+                                <a href="signin.php" class="signinV nav-link text-left ">Sign-in/</a>
+                                <a href="signup.php" class="signupV nav-link text-left">Sign-up</a>
+                            </li>
                         </ul>                                                                                                                                                                                                                                                                                          </ul>
                     </nav>
 
