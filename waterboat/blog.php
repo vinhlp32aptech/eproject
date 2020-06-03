573d051b75ad7c1ecff02b1174161ba943503204
vinhlp32aptech <vinh.lp32@aptechlearning.edu.vn>
2020-06-03 14:22:52 +0700

    sua header

--- waterboat/blog.php
+++ waterboat/blog.php
@@ -34,108 +34,8 @@
 
   <div class="site-wrap">
 
-    <div class="site-mobile-menu site-navbar-target">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
+      <?php include_once "public/header.php" ?>
 
-
-    
-    <div class="header-top bg-light">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-6 col-lg-3">
-            <a href="index.php">
-              <img src="images/logo.png" alt="Image" class="img-fluid">
-              <!-- <strong>Water</strong>Boat -->
-            </a>
-          </div>
-          <div class="col-lg-3 d-none d-lg-block">
-
-            <div class="quick-contact-icons d-flex">
-              <div class="icon align-self-start">
-                <span class="icon-location-arrow text-primary"></span>
-              </div>
-              <div class="text">
-                <span class="h4 d-block">San Francisco</span>
-                <span class="caption-text">Mountain View, Fake st., CA</span>
-              </div>
-            </div>
-
-          </div>
-          <div class="col-lg-3 d-none d-lg-block">
-            <div class="quick-contact-icons d-flex">
-              <div class="icon align-self-start">
-                <span class="icon-phone text-primary"></span>
-              </div>
-              <div class="text">
-                <span class="h4 d-block">000 209 392 312</span>
-                <span class="caption-text">Toll free</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 d-none d-lg-block">
-            <div class="quick-contact-icons d-flex">
-              <div class="icon align-self-start">
-                <span class="icon-envelope text-primary"></span>
-              </div>
-              <div class="text">
-                <span class="h4 d-block">info@gmail.com</span>
-                <span class="caption-text">Gournadi, 1230 Bariasl</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-6 d-block d-lg-none text-right">
-              <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
-                class="icon-menu h3"></span></a>
-          </div>
-        </div>
-      </div>
-      
-
-
-      
-      <div class="site-navbar py-2 js-sticky-header site-navbar-target d-none pl-0 d-lg-block" role="banner">
-
-      <div class="container">
-        <div class="d-flex align-items-center">
-          
-          <div class="mx-auto">
-            <nav class="site-navigation position-relative text-right" role="navigation">
-              <ul class="site-menu main-menu js-clone-nav mr-auto d-none pl-0 d-lg-block">
-                <li>
-                  <a href="index.php" class="nav-link text-left">Home</a>
-                </li>
-                <li>
-                  <a href="about.php" class="nav-link text-left">About Us</a>
-                </li>
-                <li>
-                  <a href="services.php" class="nav-link text-left">Services</a>
-                </li>
-                <li>
-                    <a href="testimonials.php" class="nav-link text-left">Testimonials</a>
-                </li>
-                <li class="active"><a href="blog.php" class="nav-link text-left">Blog</a></li>
-                <li>
-                    <a href="contact.php" class="nav-link text-left">Contact</a>
-                  </li>
-              </ul>                                                                                                                                                                                                                                                                                          </ul>
-            </nav>
-
-          </div>
-         
-        </div>
-      </div>
-
-    </div>
-    
-    </div>
     
     
 
@@ -236,59 +136,8 @@
         </div>
     </div>
 
-    
-    <div class="footer bg-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3">
-            <p class="mb-4"><img src="images/logo.png" alt="Image" class="img-fluid"></p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nemo minima qui dolor, iusto iure.</p>  
-            <p><a href="#">Learn More</a></p>
-          </div>
-          <div class="col-lg-3">
-            <h3 class="footer-heading"><span>Our Company</span></h3>
-            <ul class="list-unstyled">
-                <li><a href="#">About</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Our Team</a></li>
-                <li><a href="#">Careers</a></li>
-                <li><a href="#">Projects</a></li>
-            </ul>
-          </div>
-          <div class="col-lg-3">
-              <h3 class="footer-heading"><span>Our Services</span></h3>
-              <ul class="list-unstyled">
-                  <li><a href="#">Industrial</a></li>
-                  <li><a href="#">Construction</a></li>
-                  <li><a href="#">Remodeling</a></li>
-              </ul>
-          </div>
-          <div class="col-lg-3">
-              <h3 class="footer-heading"><span>Contact</span></h3>
-              <ul class="list-unstyled">
-                  <li><a href="#">Help Center</a></li>
-                  <li><a href="#">Support Community</a></li>
-                  <li><a href="#">Press</a></li>
-                  <li><a href="#">FAQ</a></li>
-                  <li><a href="#">Our Partners</a></li>
-              </ul>
-          </div>
-        </div>
 
-        <div class="row">
-          <div class="col-12">
-            <div class="copyright">
-                <p>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      <?php include_once "public/footer.php" ?>
     
 
   </div>
