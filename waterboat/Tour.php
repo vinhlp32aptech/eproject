a03b4e1584cd53c4b439f6b66e2bbc4510e6fe62
nguyentuanquang <quang.nt36@aptechlearning.edu.vn>
2020-06-03 16:32:27 +0700

    ppp

---
+++ waterboat/Tour.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<main class="hoc container clear">
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <article class="group btmspace-80">
+        <div class="two_third first"><img class="borderedbox inspace-10" src="images/demo/660x360.png" alt=""></div>
+        <div class="one_third">
+            <h6 class="heading">Per inceptos himenaeos donec lacinia mollis vel</h6>
+            <ul class="nospace meta">
+                <li><i class="fa fa-user"></i> <a href="#">Admin</a></li>
+                <li><i class="fa fa-tag"></i> <a href="#">Tag Name</a></li>
+            </ul>
+            <p>Aliquam mauris morbi tristique orci sit amet sapien tincidunt ut rutrum dui tincidunt.</p>
+            <p class="btmspace-30">Cras eget lacinia magna nunc ut est est cras aliquam erat sem at dapibus lorem luctus sed nunc sagittis leo in&hellip;</p>
+            <footer class="nospace"><a class="btn" href="#">Full Story &raquo;</a></footer>
+        </div>
+    </article>
+    <!-- ################################################################################################ -->
+    <hr class="btmspace-80">
+    <!-- ################################################################################################ -->
+    <ul class="nospace group overview">
+        <li class="one_third">
+            <article><a href="#"><img src="images/demo/320x240.png" alt=""></a>
+                <h6 class="heading">Metus porta fringilla</h6>
+                <ul class="nospace meta">
+                    <li><i class="fa fa-user"></i> <a href="#">Admin</a></li>
+                    <li><i class="fa fa-tag"></i> <a href="#">Tag Name</a></li>
+                </ul>
+                <p>Vestibulum porttitor rhoncus mi porta consectetur dui consequat vitae vivamus vitae urna quis urna pulvinar&hellip;</p>
+                <footer class="nospace"><a class="btn" href="#">Full Story &raquo;</a></footer>
+            </article>
+        </li>
+        <li class="one_third">
+            <article><a href="#"><img src="images/demo/320x240.png" alt=""></a>
+                <h6 class="heading">Suspendisse sollicitudin</h6>
+                <ul class="nospace meta">
+                    <li><i class="fa fa-user"></i> <a href="#">Admin</a></li>
+                    <li><i class="fa fa-tag"></i> <a href="#">Tag Name</a></li>
+                </ul>
+                <p>Lorem a mauris accumsan iaculis rutrum libero aenean fringilla risus eu varius blandit donec convallis nunc et&hellip;</p>
+                <footer class="nospace"><a class="btn" href="#">Full Story &raquo;</a></footer>
+            </article>
+        </li>
+        <li class="one_third">
+            <article><a href="#"><img src="images/demo/320x240.png" alt=""></a>
+                <h6 class="heading">Lacus ultricies ac luctus</h6>
+                <ul class="nospace meta">
+                    <li><i class="fa fa-user"></i> <a href="#">Admin</a></li>
+                    <li><i class="fa fa-tag"></i> <a href="#">Tag Name</a></li>
+                </ul>
+                <p>Neque tincidunt vestibulum finibus efficitur nisi sit amet elementum lorem efficitur eget habitant tristique&hellip;</p>
+                <footer class="nospace"><a class="btn" href="#">Full Story &raquo;</a></footer>
+            </article>
+        </li>
+        <li class="one_third">
+            <article><a href="#"><img src="images/demo/320x240.png" alt=""></a>
+                <h6 class="heading">Senectus malesuada ac</h6>
+                <ul class="nospace meta">
+                    <li><i class="fa fa-user"></i> <a href="#">Admin</a></li>
+                    <li><i class="fa fa-tag"></i> <a href="#">Tag Name</a></li>
+                </ul>
+                <p>Turpis egestas aenean commodo imperdiet ligula eget varius morbi quis scelerisque orci ut consequat augue nulla&hellip;</p>
+                <footer class="nospace"><a class="btn" href="#">Full Story &raquo;</a></footer>
+            </article>
+        </li>
+        <li class="one_third">
+            <article><a href="#"><img src="images/demo/320x240.png" alt=""></a>
+                <h6 class="heading">Porttitor elit vel lorem</h6>
+                <ul class="nospace meta">
+                    <li><i class="fa fa-user"></i> <a href="#">Admin</a></li>
+                    <li><i class="fa fa-tag"></i> <a href="#">Tag Name</a></li>
+                </ul>
+                <p>Sollicitudin ut suscipit justo hendrerit morbi semper purus at dapibus pharetra nulla et tristique nulla donec&hellip;</p>
+                <footer class="nospace"><a class="btn" href="#">Full Story &raquo;</a></footer>
+            </article>
+        </li>
+        <li class="one_third">
+            <article><a href="#"><img src="images/demo/320x240.png" alt=""></a>
+                <h6 class="heading">Lobortis enim placerat</h6>
+                <ul class="nospace meta">
+                    <li><i class="fa fa-user"></i> <a href="#">Admin</a></li>
+                    <li><i class="fa fa-tag"></i> <a href="#">Tag Name</a></li>
+                </ul>
+                <p>Est consequat non vestibulum quis tortor nulla cras a condimentum dolor nulla porttitor dolor id feugiat quisque&hellip;</p>
+                <footer class="nospace"><a class="btn" href="#">Full Story &raquo;</a></footer>
+            </article>
+        </li>
+    </ul>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+</main>
+</body>
+</html>
--- waterboat/services.php
+++ waterboat/services.php
@@ -24,8 +24,8 @@
   <link rel="stylesheet" href="css/aos.css">
   <link href="css/jquery.mb.YTPlayer.min.css" media="all" rel="stylesheet" type="text/css">
 
-  <link rel="stylesheet" href="css/style.css">
-
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/services.css">
 
 
 </head>
@@ -35,8 +35,6 @@
   <div class="site-wrap">
 
       <?php include_once "public/header.php" ?>
-    
-    
 
       <div class="intro-section" style="background-image: url('images/hero_2.jpg');">
         <div class="container">
@@ -49,152 +47,158 @@
           </div>
         </div>
       </div>
-
-    
-
-    <div class="py-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-yacht"></span>
-              </span>
-              <h3>Luxuries Yacht</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-shield"></span>
-              </span>
-              <h3>30 Years of Experience</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-captain"></span>
-              </span>
-              <h3>Good Captain</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
-          </div>
-
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-devices"></span>
-              </span>
-              <h3>Luxuries Yacht</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-badge"></span>
-              </span>
-              <h3>30 Years of Experience</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-5">
-            <div class="service-29283">
-              <span class="wrap-icon-39293">
-                <span class="flaticon-captain-1"></span>
-              </span>
-              <h3>Good Captain</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ipsa, ad ratione quos distinctio unde.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    
-    <div class="site-section">
-      <div class="container">
-        <div class="row justify-content-center mb-5">
-          <div class="col-md-7 text-center">
-            <span class="text-serif text-primary">Testimonial</span>
-            <h3 class="heading-92913 text-black text-center">What Customer Saying...</h3>
-          </div>
-        </div>
-        <div class="row">
-          <div class="mb-4 mb-lg-0 col-md-6 col-lg-4">
-            <div class="testimony-39291">
-              <blockquote>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem laborum cumque commodi, unde doloribus.</p>
-              </blockquote>
-              <div class="d-flex vcard align-items-center">
-                <div class="pic mr-3">
-                  <img src="images/person_3_sq.jpg" alt="Image" class="img-fluid">
-                </div>
-                <div class="text">
-                  <strong class="d-block">John Doe</strong>
-                  <span>CEO and Co-Founder</span>
-                </div>
+      <div class="popular-location section-padding30">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-12">
+                      <!-- Section Tittle -->
+                      <div class="section-tittle text-center mb-80">
+                          <span>Most visited places</span>
+                          <h2>Popular Locations</h2>
+                      </div>
+                  </div>
               </div>
-            </div>
-          </div>
-          <div class="mb-4 mb-lg-0 col-md-6 col-lg-4">
-            <div class="testimony-39291">
-              <blockquote>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem laborum cumque commodi, unde doloribus.</p>
-              </blockquote>
-              <div class="d-flex vcard align-items-center">
-                <div class="pic mr-3">
-                  <img src="images/person_4_sq.jpg" alt="Image" class="img-fluid">
-                </div>
-                <div class="text">
-                  <strong class="d-block">John Doe</strong>
-                  <span>CEO and Co-Founder</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="mb-4 mb-lg-0 col-md-6 col-lg-4">
-            <div class="testimony-39291">
-              <blockquote>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem laborum cumque commodi, unde doloribus.</p>
-              </blockquote>
-              <div class="d-flex vcard align-items-center">
-                <div class="pic mr-3">
-                  <img src="images/person_3_sq.jpg" alt="Image" class="img-fluid">
-                </div>
-                <div class="text">
-                  <strong class="d-block">John Doe</strong>
-                  <span>CEO and Co-Founder</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-    <div class="site-section bg-image overlay" style="background-image: url('images/hero_2.jpg');">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-7 text-center">
-            <h2 class="text-white">Get In Touch With Us</h2>
-            <p class="lead text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            <p class="mb-0"><a href="#" class="btn btn-warning py-3 px-5 text-white">Contact Us</a></p>
-          </div>
-        </div>
-      </div>
-    </div>
+              <div class="row">
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/new_york.jpg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>New York</p>
+                              <a href="#" class="location-btn">65 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/phap.png" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>Paris</p>
+                              <a href="#" class="location-btn">60 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/Rome.jpg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>Rome</p>
+                              <a href="#" class="location-btn">50 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/italia.jpg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>Italy</p>
+                              <a href="#" class="location-btn">28 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/Vietna.jpeg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>Vietnam</p>
+                              <a href="#" class="location-btn">99 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/indonesia.jpg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>indonesia</p>
+                              <a href="#" class="location-btn">78 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/indonesia.jpg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>indonesia</p>
+                              <a href="#" class="location-btn">78 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
 
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/indonesia.jpg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>indonesia</p>
+                              <a href="#" class="location-btn">78 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
 
-      <?php include_once "public/footer.php" ?>
-    
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/indonesia.jpg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>indonesia</p>
+                              <a href="#" class="location-btn">78 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
 
-  </div>
-  <!-- .site-wrap -->
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/indonesia.jpg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>indonesia</p>
+                              <a href="#" class="location-btn">78 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
 
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/indonesia.jpg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>indonesia</p>
+                              <a href="#" class="location-btn">78 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="col-lg-4 col-md-6 col-sm-6">
+                      <div class="single-location mb-30">
+                          <div class="location-img">
+                              <img src="images/indonesia.jpg" alt="" height="360px" width="286px">
+                          </div>
+                          <div class="location-details">
+                              <p>indonesia</p>
+                              <a href="#" class="location-btn">78 <i class="ti-plus"></i> Location</a>
+                          </div>
+                      </div>
+                  </div>
+</div>
+                  <!-- .site-wrap -->
+kkjhjk
 
   <!-- loader -->
   <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#ff5e15"/></svg></div>
@@ -217,7 +221,8 @@
 
 
 
-  <script src="js/main.js"></script>
+          <script src="js/main.js"></script>
+          <script src="js/sevices.js"></script>
 
 </body>
 
