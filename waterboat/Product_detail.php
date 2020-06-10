7222b5955719462edca7650ace5c14ea2dbdbc42
vinhlp32aptech <vinh.lp32@aptechlearning.edu.vn>
2020-06-10 14:53:20 +0700

    tabs trong product detail

--- waterboat/Product_detail.php
+++ waterboat/Product_detail.php
@@ -75,208 +75,222 @@
    <div class="container">
 
 
+       <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+           <li class="nav-item" role="presentation">
+               <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Info</a>
+           </li>
+           <li class="nav-item" role="presentation">
+               <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Images</a>
+           </li>
+       </ul>
+       <div class="tab-content" id="pills-tabContent">
+           <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+               <div class="row">
+                   <aside class="col-md-8"><h1>Azimut Magellano 43, 2014, Croatia</h1> <n/>
+                       <p>The most popular line of Azimut Magellano Cruises, designed by a typical yacht designer Cor D. Rover. Magellano 43 stands out for its abilities when you do not just stop by going in the calm waters, but also can travel on the remote seas and large waves with your Magellano.
 
-       <div class="row">
-           <aside class="col-md-8"><h1>Azimut Magellano 43, 2014, Croatia</h1> <n/>
-               <p>The most popular line of Azimut Magellano Cruises, designed by a typical yacht designer Cor D. Rover. Magellano 43 stands out for its abilities when you do not just stop by going in the calm waters, but also can travel on the remote seas and large waves with your Magellano.
+                           3 Cabins + 2 spacious bathrooms with separate standing bath (very noticeable on the size cruise), 4 lougne area/dining table around the boat. High-altitude roadblocks give you a sense of comfort and certainty
 
-                   3 Cabins + 2 spacious bathrooms with separate standing bath (very noticeable on the size cruise), 4 lougne area/dining table around the boat. High-altitude roadblocks give you a sense of comfort and certainty
+                           This Magellano 43 with 1 unique, well-maintained home by the original Azimut team.
 
-                   This Magellano 43 with 1 unique, well-maintained home by the original Azimut team.
+                           <strong>Call us now for more details!</strong></p><n/>
 
-                   <strong>Call us now for more details!</strong></p><n/>
-
-               <h1>Featured Features</h1><n/>
-              <ul>
-                  <li>Excellent yacht status</li>
-                  <li>Joystick for easy docked</li>
-                  <li>3rd Cabin</li>
-                  <li>Machine canoe with Yamaha engine set apart. </li>
-              </ul>
-<br/>
-               <h1>Installed devices</h1><n/>
-               <h5>ADVANCED PACKAGE</h5>
-               <ul>
-                   <li>11Kw Generator </li>
-                   <li>Air conditioning 40 000 BTU </li>
-                   <li>Double-Filter for motors </li>
-                   <li>6.3 Kw Motor Boat Propulsion</li>
-                   <li>Emergency pumps in a compartment </li>
-                   <li>Filters for the generator </li>
-                   <li>Wiring path waiting to be available from the cabin/driving station/Fly </li>
-                   <li>XENTA joystick for engine and propulsion</li>
-                   <li>Automatic Fire extinguishing system </li>
-                   <li>Chain Washing system </li>
-                   <li>Electronic search Lamp</li>
-                   <li>Outer sheath Set </li>
-                   <li>Open outer fence to open the boarding aisle </li>
-                   <li>Shore lines </li>
-                   <li>Control lamp from main helm </li>
-                   <li>"Exclusive" Decoration package </li>
-                   <li>Miele Kitchen Appliances </li>
-               </ul>
-               <br/>
-               <h5>ADDITIONAL PAGES </h5>
-               <ul>
-                   <li>Roof for stern</li>
-                   <li>Hull for the stern </li>
-                   <li>Cockpit table </li>
-                   <li>Bimini on Fly </li>
-                   <li>Lifting table with sun loungers in the bow</li>
-                   <li>Teak on swimming floor </li>
-                   <li>Teak on Tail Board </li>
-                   <li>Road boarding the lifting ship </li>
-                   <li>Mosquito net</li>
-                   <li>3rd Cabin (Custom) </li>
-                   <li>Fridge and cooler Cabinet 195LT </li>
-                   <li>Refrigerators on the Fly </li>
-                   <li>Raymarine Silver Package </li>
-                   <li>TV with antenna </li>
-                   <li>3 TV Philip </li>
-                   <li>Canoe Capelli with Yamaha F6 4-stroke</li>
-               </ul>
-
-           </aside>
-           <aside class="col-4">
-           <div class="container">
-               <div class="backgruod">
-                   <div class="style">
-               <table class=" table-hover">
-                   <tbody>
-                   <tr>
-                       <th class="col-2"><h1>Sell:</h1></th>
-
-                       <td class="col-2"><h1> <font color="#F00000">$416.668</font> </h1></td>
-                   </tr>
-                   </tbody>
-               </table>
-                       </div>
-               <table class="table table-condensed">
-                       <thead>
-                       <tr>
-                           <th>Length</th>
-                           <th>Cabin</th>
-                           <th>Year</th>
-
-                       </tr>
-                       </thead>
-                       <tbody>
-                       <tr>
-                           <td>13.63</td>
-                           <td>2</td>
-                           <td>2014</td>
-                       </tr>
-                       <tbody/>
-
-               </table>
-
-               </div>
-               <p><h2>Specification</h2></p>
+                       <h1>Featured Features</h1><n/>
+                       <ul>
+                           <li>Excellent yacht status</li>
+                           <li>Joystick for easy docked</li>
+                           <li>3rd Cabin</li>
+                           <li>Machine canoe with Yamaha engine set apart. </li>
+                       </ul>
                        <br/>
+                       <h1>Installed devices</h1><n/>
+                       <h5>ADVANCED PACKAGE</h5>
+                       <ul>
+                           <li>11Kw Generator </li>
+                           <li>Air conditioning 40 000 BTU </li>
+                           <li>Double-Filter for motors </li>
+                           <li>6.3 Kw Motor Boat Propulsion</li>
+                           <li>Emergency pumps in a compartment </li>
+                           <li>Filters for the generator </li>
+                           <li>Wiring path waiting to be available from the cabin/driving station/Fly </li>
+                           <li>XENTA joystick for engine and propulsion</li>
+                           <li>Automatic Fire extinguishing system </li>
+                           <li>Chain Washing system </li>
+                           <li>Electronic search Lamp</li>
+                           <li>Outer sheath Set </li>
+                           <li>Open outer fence to open the boarding aisle </li>
+                           <li>Shore lines </li>
+                           <li>Control lamp from main helm </li>
+                           <li>"Exclusive" Decoration package </li>
+                           <li>Miele Kitchen Appliances </li>
+                       </ul>
+                       <br/>
+                       <h5>ADDITIONAL PAGES </h5>
+                       <ul>
+                           <li>Roof for stern</li>
+                           <li>Hull for the stern </li>
+                           <li>Cockpit table </li>
+                           <li>Bimini on Fly </li>
+                           <li>Lifting table with sun loungers in the bow</li>
+                           <li>Teak on swimming floor </li>
+                           <li>Teak on Tail Board </li>
+                           <li>Road boarding the lifting ship </li>
+                           <li>Mosquito net</li>
+                           <li>3rd Cabin (Custom) </li>
+                           <li>Fridge and cooler Cabinet 195LT </li>
+                           <li>Refrigerators on the Fly </li>
+                           <li>Raymarine Silver Package </li>
+                           <li>TV with antenna </li>
+                           <li>3 TV Philip </li>
+                           <li>Canoe Capelli with Yamaha F6 4-stroke</li>
+                       </ul>
 
-                       <table class="table table-striped">
-                        <tbody>
-                        <tr>
-                            <th>Model</th>
-                            <td>Azimut Magellano 43</td>
-                        </tr>
-                        <tr>
-                            <th>Year</th>
-                            <td>2014</td>
-                        </tr>
-                        <tr>
-                            <th>Engine hours</th>
-                            <td> 430 h</td>
-                        </tr>
-                        <tr>
-                            <th>Total length</th>
-                            <td> 13.63  m</td>
-                        </tr>
-                        <tr>
-                            <th>Hull length</th>
-                            <td> 12.96 m</td>
-                        </tr>
-                        <tr>
-                            <th>Horizontally</th>
-                            <td> 4.40 m</td>
-                        </tr>
-                        <tr>
-                            <th>Draft max</th>
-                            <td>1.20 m</td>
-                        </tr>
-                        <tr>
-                            <th>Turn water intake</th>
-                            <td> 17.40 t</td>
-                        </tr>
-                        <tr>
-                            <th>Fuel tank</th>
-                            <td>1680 l</td>
-                        </tr>
-                        <tr>
-                            <th>Water tank</th>
-                            <td>600 l</td>
-                        </tr>
-                        <tr>
-                            <th>Black Water tank</th>
-                            <td>130  l</td>
-                        </tr>
-                        <tr>
-                            <th>Grey Water tank</th>
-                            <td>40  l</td>
-                        </tr>
-                        <tr>
-                            <th>Speed of fuel economy</th>
-                            <td>16 NMI</td>
-                        </tr>
-                        <tr>
-                            <th>Max speed, nautical knots</th>
-                            <td> 22 NMI</td>
-                        </tr>
-                        <tr>
-                            <th>Motor</th>
-                            <td> 2 x 355 hp Cummins QSB 5.9 – V-drive</td>
-                        </tr>
-                        <tr>
-                            <th>Production materials</th>
-                            <td>VTR/GRP</td>
-                        </tr>
-                        <tr>
-                            <th>Exterior design</th>
-                            <td> Cor D. Rover Yacht Design</td>
-                        </tr>
-                        <tr>
-                            <th>Interior Design</th>
-                            <td> Azimut Yachts</td>
-                        </tr>
-                        <tr>
-                            <th>Cabin</th>
-                            <td>2</td>
-                        </tr>
-                        <tr>
-                            <th>Sleeping Place</th>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <th>Locations</th>
-                            <td> Croatia</td>
-                        </tr>
-                        <tr>
-                            <th>Flag</th>
-                            <td> Jersey</td>
-                        </tr>
-                        <tr>
-                            <th>VAT paid or not, which country</th>
-                            <td>Not paid</td>
-                        </tr>
-                        <tr>
-                            <th>Shipbuilding</th>
-                            <td> Azimut Yachts (Italy)</td>
-                        </tr>
+                   </aside>
+                   <aside class="col-4">
+                       <div class="container">
+                           <div class="backgruod">
+                               <div class="style">
+                                   <table class=" table-hover">
+                                       <tbody>
+                                       <tr>
+                                           <th class="col-2"><h1>Sell:</h1></th>
 
-                        </tbody>
+                                           <td class="col-2"><h1> <font color="#F00000">$416.668</font> </h1></td>
+                                       </tr>
+                                       </tbody>
+                                   </table>
+                               </div>
+                               <table class="table table-condensed">
+                                   <thead>
+                                   <tr>
+                                       <th>Length</th>
+                                       <th>Cabin</th>
+                                       <th>Year</th>
 
-                       </table>
-</div>
+                                   </tr>
+                                   </thead>
+                                   <tbody>
+                                   <tr>
+                                       <td>13.63</td>
+                                       <td>2</td>
+                                       <td>2014</td>
+                                   </tr>
+                                   <tbody/>
+
+                               </table>
+
+                           </div>
+                           <p><h2>Specification</h2></p>
+                           <br/>
+
+                           <table class="table table-striped">
+                               <tbody>
+                               <tr>
+                                   <th>Model</th>
+                                   <td>Azimut Magellano 43</td>
+                               </tr>
+                               <tr>
+                                   <th>Year</th>
+                                   <td>2014</td>
+                               </tr>
+                               <tr>
+                                   <th>Engine hours</th>
+                                   <td> 430 h</td>
+                               </tr>
+                               <tr>
+                                   <th>Total length</th>
+                                   <td> 13.63  m</td>
+                               </tr>
+                               <tr>
+                                   <th>Hull length</th>
+                                   <td> 12.96 m</td>
+                               </tr>
+                               <tr>
+                                   <th>Horizontally</th>
+                                   <td> 4.40 m</td>
+                               </tr>
+                               <tr>
+                                   <th>Draft max</th>
+                                   <td>1.20 m</td>
+                               </tr>
+                               <tr>
+                                   <th>Turn water intake</th>
+                                   <td> 17.40 t</td>
+                               </tr>
+                               <tr>
+                                   <th>Fuel tank</th>
+                                   <td>1680 l</td>
+                               </tr>
+                               <tr>
+                                   <th>Water tank</th>
+                                   <td>600 l</td>
+                               </tr>
+                               <tr>
+                                   <th>Black Water tank</th>
+                                   <td>130  l</td>
+                               </tr>
+                               <tr>
+                                   <th>Grey Water tank</th>
+                                   <td>40  l</td>
+                               </tr>
+                               <tr>
+                                   <th>Speed of fuel economy</th>
+                                   <td>16 NMI</td>
+                               </tr>
+                               <tr>
+                                   <th>Max speed, nautical knots</th>
+                                   <td> 22 NMI</td>
+                               </tr>
+                               <tr>
+                                   <th>Motor</th>
+                                   <td> 2 x 355 hp Cummins QSB 5.9 – V-drive</td>
+                               </tr>
+                               <tr>
+                                   <th>Production materials</th>
+                                   <td>VTR/GRP</td>
+                               </tr>
+                               <tr>
+                                   <th>Exterior design</th>
+                                   <td> Cor D. Rover Yacht Design</td>
+                               </tr>
+                               <tr>
+                                   <th>Interior Design</th>
+                                   <td> Azimut Yachts</td>
+                               </tr>
+                               <tr>
+                                   <th>Cabin</th>
+                                   <td>2</td>
+                               </tr>
+                               <tr>
+                                   <th>Sleeping Place</th>
+                                   <td>4</td>
+                               </tr>
+                               <tr>
+                                   <th>Locations</th>
+                                   <td> Croatia</td>
+                               </tr>
+                               <tr>
+                                   <th>Flag</th>
+                                   <td> Jersey</td>
+                               </tr>
+                               <tr>
+                                   <th>VAT paid or not, which country</th>
+                                   <td>Not paid</td>
+                               </tr>
+                               <tr>
+                                   <th>Shipbuilding</th>
+                                   <td> Azimut Yachts (Italy)</td>
+                               </tr>
+
+                               </tbody>
+
+                           </table>
+                       </div>
+               </div>
+           </div>
+           <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+               <img src="images/3.jpeg" alt="">
+           </div>
        </div>
        <div id="loader" class="show fullscreen">
            <svg class="circular" width="48px" height="48px">
