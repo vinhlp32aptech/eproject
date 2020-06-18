c27d29d57294f47050c4133c008c1fccc0cfd352
vinhlp32aptech <vinh.lp32@aptechlearning.edu.vn>
2020-06-18 19:42:14 +0700

    ad

--- waterboat/admin/admin.php
+++ waterboat/admin/admin.php
@@ -4,44 +4,7 @@ include_once "../conn/database.php";
 include_once "../conn/Pagination.php";
 $db = new database();
 
-//show data, search and paging
-if(isset($_GET['search'])):
-    $query ="select * from product where status = 1 && concat(status,name_pro,price_pro,year_pro,code) like ? ";
-    $param = [
-        "%{$_GET['search']}%"
-    ];
-    $stmt = $db->selectDataParam($query, $param);
-    $total = $stmt->rowCount();
-    $config = [
-        'current_page'  => isset($_GET['page'])?$_GET['page']: 1, // Trang hiện tại
-        'total_record'  => $total, // Tổng số record -> tong so hang
-        'limit'         => 10,// limit
-        'link_full'     => (trim($_GET['search'])=="")?'admin.php?page={page}':"admin.php?page={page}&search={$_GET['search']}",// Link full có dạng như sau: domain/com/page/{page}
-        'link_first'    => (trim($_GET['search'])=="")?'admin.php':"admin.php?search={$_GET['search']}",// Link trang đầu tiên
-        'range'         => 5, // Số button trang bạn muốn hiển thị
-    ];
-    $paging = new Pagination();
-    $paging->init($config);
-    $query = "select * from product where status = 1 && concat(status,name_pro,price_pro,year_pro,code) like ? " .$paging->get_limit();
-    $stmt = $db->selectdataparam($query,$param);
-else:
 
-    $query = "select * from product where status = 1 ";
-    $stmt = $db->selectData($query);
-    $total = $stmt->rowCount();
-    $config = [
-        'current_page'  => isset($_GET['page'])?$_GET['page']: 1, // Trang hiện tại
-        'total_record'  => $total, // Tổng số record -> tong so hang
-        'limit'         => 10,// limit
-        'link_full'     => 'admin.php?page={page}',// Link full có dạng như sau: domain/com/page/{page}
-        'link_first'    => 'admin.php',// Link trang đầu tiên
-        'range'         => 5, // Số button trang bạn muốn hiển thị
-    ];
-    $paging = new Pagination();
-    $paging->init($config);
-    $query = "select * from product where status = 1 " .$paging->get_limit();
-    $stmt = $db->selectdata($query);
-endif;
 //////
 //delete data-do not save
 if(isset($_POST['btndeletenow'])):
@@ -151,11 +114,52 @@ endif;
         </nav>
         <!-- END NAV -->
         <!-- CONTAINER  -->
+        <?php
+
+        //show data, search and paging
+        if(isset($_GET['search1'])):
+            $query ="select * from product where status = 1 && concat(status,name_pro,price_pro,year_pro,code) like ? ";
+            $param = [
+                "%{$_GET['search1']}%"
+            ];
+            $stmt = $db->selectDataParam($query, $param);
+            $total = $stmt->rowCount();
+            $config = [
+                'current_page'  => isset($_GET['page'])?$_GET['page']: 1, // Trang hiện tại
+                'total_record'  => $total, // Tổng số record -> tong so hang
+                'limit'         => 10,// limit
+                'link_full'     => (trim($_GET['search'])=="")?'admin.php?page={page}':"admin.php?page={page}&search={$_GET['search']}",// Link full có dạng như sau: domain/com/page/{page}
+                'link_first'    => (trim($_GET['search'])=="")?'admin.php':"admin.php?search={$_GET['search']}",// Link trang đầu tiên
+                'range'         => 5, // Số button trang bạn muốn hiển thị
+            ];
+            $paging = new Pagination();
+            $paging->init($config);
+            $query = "select * from product where status = 1 && concat(status,name_pro,price_pro,year_pro,code) like ? " .$paging->get_limit();
+            $stmt = $db->selectdataparam($query,$param);
+        else:
+
+            $query = "select * from product where status = 1 ";
+            $stmt = $db->selectData($query);
+            $total = $stmt->rowCount();
+            $config = [
+                'current_page'  => isset($_GET['page'])?$_GET['page']: 1, // Trang hiện tại
+                'total_record'  => $total, // Tổng số record -> tong so hang
+                'limit'         => 10,// limit
+                'link_full'     => 'admin.php?page={page}',// Link full có dạng như sau: domain/com/page/{page}
+                'link_first'    => 'admin.php',// Link trang đầu tiên
+                'range'         => 5, // Số button trang bạn muốn hiển thị
+            ];
+            $paging = new Pagination();
+            $paging->init($config);
+            $query = "select * from product where status = 1 " .$paging->get_limit();
+            $stmt = $db->selectdata($query);
+        ?>
+
         <div class="mainContent">
             <!-- LIST FORM -->
             <div class="row filterGroup">
                 <form action="#" method="get" class="formSearch fl">
-                    <input type="text" class="inputSearch" placeholder="Search" name="search">
+                    <input type="text" class="inputSearch" placeholder="Search" name="search1">
                     <button type="submit" class="btnSearch"><i class="fa fa-search"></i></button>
                 </form>
 <!--                <div class="areaFilter fr row">-->
@@ -249,7 +253,7 @@ endif;
                                 <input type="checkbox" class="btnSwitch status" name="status">
                             </div>
                             <div class="cell cell-100 text-center">
-                                <a href="" class="btnEdit fa fa-pencil bg-1 text-fff"></a><a href="" class="btnRemove fa fa-remove bg-1 text-fff" onclick='return confirm("Do you really want to remove it ?")'></a>
+                                <a href="admin.php?id_pro=<?=$product['id_pro'];?>" class="btnEdit fa fa-pencil bg-1 text-fff"></a><a href="" class="btnRemove fa fa-remove bg-1 text-fff" onclick='return confirm("Do you really want to remove it ?")'></a>
                             </div>
                         </li>
                     </ul>
@@ -326,7 +330,7 @@ endif;
             <?php if(isset($_GET['addpro'])):?>
                     <form action="admin.php?id_pro" method="post" enctype="multipart/form-data" class="form">
                         <div class="formHeader row">
-                            <h2 class="text-1 fl">Product Detail</h2>
+                            <h2 class="text-1 fl">Add Product</h2>
                             <div class="fr">
                                 <button type="submit" class="btnSave bg-1 text-fff text-bold fr" name="savechange">SAVE</button><a href="" class="btnAdd fa fa-plus bg-1 text-fff"></a>
                             </div>
@@ -372,11 +376,174 @@ endif;
                             </div>
                         </div>
                     </form>
-
                 <?php endif;?>
         </div>
         <!-- END CONTAINER  -->
     </div>
+    <?php endif;?>
+
+<!--    invoice-->
+    <?php
+
+    //show data, search and paging
+    if(isset($_GET['search2'])):
+        $query ="select * from invoice_details where status = 1 && concat(status,name_pro,price,date_of_purchase,phone, addr) like ? ";
+        $param = [
+            "%{$_GET['search2']}%"
+        ];
+        $stmt = $db->selectDataParam($query, $param);
+        $total = $stmt->rowCount();
+        $config = [
+            'current_page'  => isset($_GET['page'])?$_GET['page']: 1, // Trang hiện tại
+            'total_record'  => $total, // Tổng số record -> tong so hang
+            'limit'         => 10,// limit
+            'link_full'     => (trim($_GET['search'])=="")?'admin.php?page={page}':"admin.php?page={page}&search={$_GET['search']}",// Link full có dạng như sau: domain/com/page/{page}
+            'link_first'    => (trim($_GET['search'])=="")?'admin.php':"admin.php?search={$_GET['search']}",// Link trang đầu tiên
+            'range'         => 5, // Số button trang bạn muốn hiển thị
+        ];
+        $paging = new Pagination();
+        $paging->init($config);
+        $query = "select * from invoice_details where status = 1 && concat(status,name_pro,price,date_of_purchase,phone, addr) like ? " .$paging->get_limit();
+        $stmt = $db->selectdataparam($query,$param);
+//    else:
+//        if(isset($_GET['invoice'])):
+//            $query = "select * from invoice_details where status = 1 ";
+//            $stmt = $db->selectData($query);
+//            $total = $stmt->rowCount();
+//            $config = [
+//                'current_page'  => isset($_GET['page'])?$_GET['page']: 1, // Trang hiện tại
+//                'total_record'  => $total, // Tổng số record -> tong so hang
+//                'limit'         => 10,// limit
+//                'link_full'     => 'admin.php?page={page}',// Link full có dạng như sau: domain/com/page/{page}
+//                'link_first'    => 'admin.php',// Link trang đầu tiên
+//                'range'         => 5, // Số button trang bạn muốn hiển thị
+//            ];
+//            $paging = new Pagination();
+//            $paging->init($config);
+//            $query = "select * from invoice_details where status = 1 " .$paging->get_limit();
+//            $stmt = $db->selectdata($query);
+//            endif;
+    ?>
+
+    <div class="mainContent">
+        <!-- LIST FORM -->
+        <div class="row filterGroup">
+            <form action="#" method="get" class="formSearch fl">
+                <input type="text" class="inputSearch" placeholder="Search" name="search2">
+                <button type="submit" class="btnSearch"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+        <form action="" method="GET" name="listForm" class="form scrollX">
+            <div class="formHeader row">
+                <h2 class="text-1 fl">Invoice List</h2>
+                <div class="fr">
+                    <button type="submit" class="btnSave bg-1 text-fff text-bold fr">SAVE</button><a href="admin.php?addpro" class="btnAdd fa fa-plus bg-1 text-fff"></a>
+                </div>
+            </div>
+            <div class="table">
+                <div class="row bg-1">
+                    <div class="cell cell-50 text-center text-fff">ID_INVOICE</div>
+                    <div class="cell cell-50 text-center text-fff">ID_PORDUCT</div>
+                    <div class="cell cell-200 text-center text-fff">NAME</div>
+                    <div class="cell cell-100 text-center text-fff">PRICE</div>
+                    <div class="cell cell-100 text-center text-fff">QUANTITY</div>
+                    <div class="cell cell-100 text-center text-fff">DATE_OF_PURCHASE</div>
+                    <div class="cell cell-100 text-center text-fff">ADDRESS</div>
+                    <div class="cell cell-100 text-center text-fff">TOTAL</div>
+                    <div class="cell cell-100 text-center text-fff"><input type="checkbox" class="checkbox checkAll" name="statusAll" target=".status"></div>
+                    <div class="cell cell-100 text-center text-fff">EDIT</div>
+                </div>
+                <!--   BEGIN LOOP -->
+                <?php
+                while($product = $stmt->fetch(PDO::FETCH_ASSOC)):?>
+
+                    <ul>
+                        <li class="row">
+                            <div class="cell cell-50 text-center"><?=$product['id_inv'];?></div>
+                            <div class="cell cell-50 text-center"><?=$product['id_pro'];?></div>
+                            <div class="cell cell-200 text-center"><a href="admin.php?id_pro=<?=$product['id_pro'];?>"><?=$product['name_pro'];?></a></div>
+                            <div class="cell cell-100 text-center"><?=$product['price'];?></div>
+                            <div class="cell cell-100 text-center"><?=$product['quantity'];?></div>
+                            <div class="cell cell-100 text-center"><?=$product['date_of_purchase'];?></div>
+                            <div class="cell cell-100 text-center"><?=$product['addr'];?></div>
+                            <div class="cell cell-100 text-center"><?=$product['total'];?></div>
+
+                            <div class="cell cell-100 text-center">
+                                <input type="hidden" class="status" name="status" value="0">
+                                <input type="checkbox" class="btnSwitch status" name="status">
+                            </div>
+                            <div class="cell cell-100 text-center">
+                                <a href="admin.php?id_pro=<?=$product['id_pro'];?>" class="btnEdit fa fa-pencil bg-1 text-fff"></a><a href="" class="btnRemove fa fa-remove bg-1 text-fff" onclick='return confirm("Do you really want to remove it ?")'></a>
+                            </div>
+                        </li>
+                    </ul>
+                <?php endwhile; ?>
+
+                <!--   END LOOP -->
+            </div>
+        </form>
+        <?php if(isset($paging)):
+            echo $paging->html();
+        endif; ?>
+
+        <!-- DETAIL FORM -->
+        <?php
+        if(isset($_GET['id_pro'])):
+            $query =  "select * from product where id_pro = :id_pro";
+            $param = [
+                "id_pro"=>$_GET['id_pro']
+            ];
+
+            $stmt = $db->selectdataparam($query, $param);
+        endif;
+        while($product = $stmt->fetch(PDO::FETCH_ASSOC)):?>
+
+            <form action="admin.php?id_pro=<?=$product['id_pro'];?>" method="post" enctype="multipart/form-data" class="form">
+                <div class="formHeader row">
+                    <h2 class="text-1 fl">Invoice Detail</h2>
+                    <div class="fr">
+                        <button type="submit" class="btnSave bg-1 text-fff text-bold fr" name="savechange">SAVE</button><a href="" class="btnAdd fa fa-plus bg-1 text-fff"></a>
+                    </div>
+                </div>
+
+                <div class="formBody row">
+                    <div class="column s-6">
+                        <label class="inputGroup">
+                            <span>Name</span>
+                            <span><input type="text" name="name_pro" value="<?=$product['name_pro'];?>"></span>
+                        </label>
+                        <label class="inputGroup">
+                            <span>Price</span>
+                            <span><input type="number" name="price_pro" value="<?=$product['price'];?>"></span>
+                        </label>
+                        <label class="inputGroup">
+                            <span>Quantity</span>
+                            <span><input type="number" name="quantity_pro" value="<?=$product['quantity'];?>"></span>
+                        </label>
+                        <label class="inputGroup">
+                            <span>Date of Purchase</span>
+                            <span><input type="date" name="data_of_purchase" value="<?=$product['data_of_purchase'];?>"></span>
+                        </label>
+                    </div>
+                    <div class="column s-6">
+                        <label class="inputGroup">
+                            <span>Address</span>
+                            <span><input type="text" name="code" value="<?=$product['addr'];?>"></span>
+                        </label>
+                        <label class="inputGroup">
+                            <span>Total</span>
+                            <span><input type="text" name="status_pro" value="<?=$product['total'];?>"></span>
+                        </label>
+                    </div>
+                </div>
+            </form>
+        <?php endwhile;$db->closeconnect();?>
+    </div>
+    <!-- END CONTAINER  -->
+</div>
+<?php endif;?>
+
+
 </div>
 
 
